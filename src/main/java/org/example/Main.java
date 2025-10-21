36bf0197eed279a14b341c133e95b25d553ed035
Aaron-JByrne <25444379@studentmail.ul.ie>
2025-10-21 11:32:51 +0100

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.GlobalMemory;
+import oshi.software.os.OperatingSystem;
+
+public class Main {
+    public static void main(String[] args) {
+        SystemInfo si = new SystemInfo();
+        OperatingSystem os = si.getOperatingSystem();
+        CentralProcessor cpu = si.getHardware().getProcessor();
+        GlobalMemory memory = si.getHardware().getMemory();
+
+        System.out.println("OS: " + os);
+        System.out.println("Kernel Version: " + os.getVersionInfo().getVersion());
+        System.out.println("CPU: " + cpu.getProcessorIdentifier().getName());
+        System.out.println("Cores: " + cpu.getLogicalProcessorCount());
+        System.out.printf("Memory: %.2f GB total, %.2f GB used%n",
+                memory.getTotal() / 1e9,
+                (memory.getTotal() - memory.getAvailable()) / 1e9);
+    }
+}
