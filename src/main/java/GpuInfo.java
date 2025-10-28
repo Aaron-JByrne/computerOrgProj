ceb8600834f82cd4dccb1f19f8352ee716d0901e
sheamascarenhas96 <sheamascarenhas96@gmail.com>
2025-10-28 09:40:45 +0000

    Update GpuInfo.java

--- src/main/java/GpuInfo.java
+++ src/main/java/GpuInfo.java
@@ -1,14 +1,13 @@
 import oshi.SystemInfo;
 import oshi.hardware.GraphicsCard;
 import oshi.hardware.HardwareAbstractionLayer;
-
 import java.util.List;
 
 public class GpuInfo {
 
-    public static void main(String[] args) {
-
-        // Make a SystemInfo object
+    // Function that prints GPU information
+    public void getGpuInformation() {
+        
         SystemInfo systemInfo = new SystemInfo();
 
         // Get hardware info
@@ -25,7 +24,7 @@ public class GpuInfo {
             System.out.println("----------------------------------");
         }
 
-        //checking if system has more than one GPU
+        // Check if system has more than one GPU
         if (gpus.size() > 1) {
             System.out.println("You have multiple GPUs!");
         } else {
@@ -33,4 +32,3 @@ public class GpuInfo {
         }
     }
 }
-
